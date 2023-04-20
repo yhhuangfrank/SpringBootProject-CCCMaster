0a694344cc4e0804c109eb61efa8539d659f5bcd
hsiang <tw19990320@gmail.com>
2023-04-21 00:49:13 +0800

    feat:product image 0421

--- src/main/java/com/ispan/CCCMaster/model/bean/weihsiang/Product.java
+++ src/main/java/com/ispan/CCCMaster/model/bean/weihsiang/Product.java
@@ -1,6 +1,7 @@
 package com.ispan.CCCMaster.model.bean.weihsiang;
 
 import com.ispan.CCCMaster.model.bean.Category;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.*;
 import java.util.List;
@@ -39,9 +40,146 @@ public class Product {
     private Integer numberOfComments;
     @Column(name = "active",nullable = false)
     private Boolean active;
-@OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "fk_product_id",referencedColumnName = "product_id")
-    private List<ProductImg> productImgs;
+    @Lob
+    @Column(name = "image", columnDefinition = "varbinary(max)")
+    private byte[] image;
+//@OneToMany(cascade = CascadeType.ALL)
+    //@JoinColumn(name = "fk_product_id",referencedColumnName = "product_id")
+    //private List<ProductImg> productImgs;
+
+    @Transient
+    private MultipartFile imageFile;
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getProductBrand() {
+        return productBrand;
+    }
+
+    public void setProductBrand(String productBrand) {
+        this.productBrand = productBrand;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Integer inventory) {
+        this.inventory = inventory;
+    }
+
+    public Integer getNumOfPurchases() {
+        return numOfPurchases;
+    }
+
+    public void setNumOfPurchases(Integer numOfPurchases) {
+        this.numOfPurchases = numOfPurchases;
+    }
+
+    public Integer getProductViews() {
+        return productViews;
+    }
+
+    public void setProductViews(Integer productViews) {
+        this.productViews = productViews;
+    }
+
+    public String getDesciption() {
+        return desciption;
+    }
+
+    public void setDesciption(String desciption) {
+        this.desciption = desciption;
+    }
+
+    public Integer getNumberOfRatings() {
+        return numberOfRatings;
+    }
+
+    public void setNumberOfRatings(Integer numberOfRatings) {
+        this.numberOfRatings = numberOfRatings;
+    }
+
+    public Double getAvgRating() {
+        return avgRating;
+    }
+
+    public void setAvgRating(Double avgRating) {
+        this.avgRating = avgRating;
+    }
+
+    public Integer getNumberOfComments() {
+        return numberOfComments;
+    }
+
+    public void setNumberOfComments(Integer numberOfComments) {
+        this.numberOfComments = numberOfComments;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    /*public List<ProductImg> getProductImgs() {
+        return productImgs;
+    }
+
+    public void setProductImgs(List<ProductImg> productImgs) {
+        this.productImgs = productImgs;
+    }*/
+
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public MultipartFile getImageFile() {
+        return imageFile;
+    }
+
+    public void setImageFile(MultipartFile imageFile) {
+        this.imageFile = imageFile;
+    }
 }
 
 
--- src/main/java/com/ispan/CCCMaster/service/ProductService.java
+++ src/main/java/com/ispan/CCCMaster/service/ProductService.java
@@ -1,13 +1,28 @@
 package com.ispan.CCCMaster.service;
 
+import com.ispan.CCCMaster.model.bean.weihsiang.Product;
 import com.ispan.CCCMaster.model.dao.ProductRepository;
+import org.apache.taglibs.standard.lang.jstl.test.beans.PublicBean1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Base64;
 
 @Service
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
+
+    public void createProduct(Product product) {
+        productRepository.save(product);
+    }
+    public String convertToBase64(MultipartFile imageFile) throws IOException {
+        byte[] bytes = imageFile.getBytes();
+        String base64 = Base64.getEncoder().encodeToString(bytes);
+        return base64;
+    }
 
 
 }
