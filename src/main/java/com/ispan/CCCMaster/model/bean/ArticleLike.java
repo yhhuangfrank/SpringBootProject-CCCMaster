e9f5bec00f361c920598da452b9b968a09dfea86
nana9669@hotmail.com <nana9669@hotmail.com>
2023-04-20 14:50:41 +0800

    OrderDetailBean和ShoppingCartDetailBean 新增id

--- src/main/java/com/ispan/CCCMaster/model/bean/ArticleLike.java
+++ src/main/java/com/ispan/CCCMaster/model/bean/ArticleLike.java
@@ -1,73 +1,73 @@
-package com.ispan.CCCMaster.model.bean;
-
-import javax.persistence.*;
-import java.util.Date;
-
-@Entity
-@Table(name = "article_like")
-public class ArticleLike {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "articleId")
-    private Integer articleId;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "forumId")
-    private Integer forumId;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "customerId")
-    private Integer customerId;
-
-    @Column(name = "like")
-    private Integer like;
-
-    @Column(name = "likeDate")
-    private Date likeDate;
-
-    public ArticleLike() {
-    }
-
-    public Integer getArticleId() {
-        return articleId;
-    }
-
-    public void setArticleId(Integer articleId) {
-        this.articleId = articleId;
-    }
-
-    public Integer getForumId() {
-        return forumId;
-    }
-
-    public void setForumId(Integer forumId) {
-        this.forumId = forumId;
-    }
-
-    public Integer getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(Integer customerId) {
-        this.customerId = customerId;
-    }
-
-    public Integer getLike() {
-        return like;
-    }
-
-    public void setLike(Integer like) {
-        this.like = like;
-    }
-
-    public Date getLikeDate() {
-        return likeDate;
-    }
-
-    public void setLikeDate(Date likeDate) {
-        this.likeDate = likeDate;
-    }
-}
+//package com.ispan.CCCMaster.model.bean;
+//
+//import javax.persistence.*;
+//import java.util.Date;
+//
+//@Entity
+//@Table(name = "article_like")
+//public class ArticleLike {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "articleId")
+//    private Integer articleId;
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "forumId")
+//    private Integer forumId;
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "customerId")
+//    private Integer customerId;
+//
+//    @Column(name = "like")
+//    private Integer like;
+//
+//    @Column(name = "likeDate")
+//    private Date likeDate;
+//
+//    public ArticleLike() {
+//    }
+//
+//    public Integer getArticleId() {
+//        return articleId;
+//    }
+//
+//    public void setArticleId(Integer articleId) {
+//        this.articleId = articleId;
+//    }
+//
+//    public Integer getForumId() {
+//        return forumId;
+//    }
+//
+//    public void setForumId(Integer forumId) {
+//        this.forumId = forumId;
+//    }
+//
+//    public Integer getCustomerId() {
+//        return customerId;
+//    }
+//
+//    public void setCustomerId(Integer customerId) {
+//        this.customerId = customerId;
+//    }
+//
+//    public Integer getLike() {
+//        return like;
+//    }
+//
+//    public void setLike(Integer like) {
+//        this.like = like;
+//    }
+//
+//    public Date getLikeDate() {
+//        return likeDate;
+//    }
+//
+//    public void setLikeDate(Date likeDate) {
+//        this.likeDate = likeDate;
+//    }
+//}
--- src/main/java/com/ispan/CCCMaster/model/bean/Response.java
+++ src/main/java/com/ispan/CCCMaster/model/bean/Response.java
@@ -1,78 +1,78 @@
-package com.ispan.CCCMaster.model.bean;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import javax.persistence.*;
-
-@Entity
-@Table(name = "response")
-public class Response {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "responseId")
-    private Integer responseId;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "customerId")
-    private Integer customerId;
-
-    @Column(name = "responseContent", columnDefinition = "nvarchar(200)", nullable = true)
-    private String responseContent;
-
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss EEEE",timezone = "GMT+8")
-    @Temporal(TemporalType.TIMESTAMP)
-    @DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss")
-    @Column(name = "responseDate", columnDefinition = "datetime")
-    private String responseDate;
-
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "articleId")
-    private Integer articleId;
-    public Response() {
-    }
-
-    public Integer getResponseId() {
-        return responseId;
-    }
-
-    public void setResponseId(Integer responseId) {
-        this.responseId = responseId;
-    }
-
-    public Integer getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(Integer customerId) {
-        this.customerId = customerId;
-    }
-
-    public String getResponseContent() {
-        return responseContent;
-    }
-
-    public void setResponseContent(String responseContent) {
-        this.responseContent = responseContent;
-    }
-
-    public String getResponseDate() {
-        return responseDate;
-    }
-
-    public void setResponseDate(String responseDate) {
-        this.responseDate = responseDate;
-    }
-
-    public Integer getArticleId() {
-        return articleId;
-    }
-
-    public void setArticleId(Integer articleId) {
-        this.articleId = articleId;
-    }
-}
+//package com.ispan.CCCMaster.model.bean;
+//
+//import com.fasterxml.jackson.annotation.JsonFormat;
+//import org.springframework.format.annotation.DateTimeFormat;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name = "response")
+//public class Response {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "responseId")
+//    private Integer responseId;
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "customerId")
+//    private Integer customerId;
+//
+//    @Column(name = "responseContent", columnDefinition = "nvarchar(200)", nullable = true)
+//    private String responseContent;
+//
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss EEEE",timezone = "GMT+8")
+//    @Temporal(TemporalType.TIMESTAMP)
+//    @DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss")
+//    @Column(name = "responseDate", columnDefinition = "datetime")
+//    private String responseDate;
+//
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "articleId")
+//    private Integer articleId;
+//    public Response() {
+//    }
+//
+//    public Integer getResponseId() {
+//        return responseId;
+//    }
+//
+//    public void setResponseId(Integer responseId) {
+//        this.responseId = responseId;
+//    }
+//
+//    public Integer getCustomerId() {
+//        return customerId;
+//    }
+//
+//    public void setCustomerId(Integer customerId) {
+//        this.customerId = customerId;
+//    }
+//
+//    public String getResponseContent() {
+//        return responseContent;
+//    }
+//
+//    public void setResponseContent(String responseContent) {
+//        this.responseContent = responseContent;
+//    }
+//
+//    public String getResponseDate() {
+//        return responseDate;
+//    }
+//
+//    public void setResponseDate(String responseDate) {
+//        this.responseDate = responseDate;
+//    }
+//
+//    public Integer getArticleId() {
+//        return articleId;
+//    }
+//
+//    public void setArticleId(Integer articleId) {
+//        this.articleId = articleId;
+//    }
+//}
