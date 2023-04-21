00f4810c1d546081d2c7d0a0ffa8ca6d4aa316a8
FrankHuang <frankcool321@gmail.com>
2023-04-21 16:49:45 +0800

    fix/add beans

---
+++ src/main/java/com/ispan/CCCMaster/model/bean/CustomerBrowsingHistory.java
@@ -0,0 +1,71 @@
+package com.ispan.CCCMaster.model.bean;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "CustomerBrowsingHistory")
+public class CustomerBrowsingHistory {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "customer_id")
+	private Integer customerId;
+	
+	@Column(name = "product_id")
+	private Integer productId;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss")
+	@Column(name = "build_time", columnDefinition = "datetime")
+	private Date buildTime;
+	
+	public CustomerBrowsingHistory() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+
+	public Integer getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+
+	public Date getBuildTime() {
+		return buildTime;
+	}
+
+	public void setBuildTime(Date buildTime) {
+		this.buildTime = buildTime;
+	}
+
+}
