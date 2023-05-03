6d29fea04cbbff98ecb46c7ee525cba60f67d264
FrankHuang <frankcool321@gmail.com>
2023-05-03 14:22:03 +0800

    refactor: 移除多餘 package import 與套件整理

--- src/main/java/com/ispan/CCCMaster/errorcontroller/PageNotFoundController.java
+++ src/main/java/com/ispan/CCCMaster/errorcontroller/PageNotFoundController.java
@@ -1,4 +1,4 @@
-package com.ispan.CCCMaster.controller.error;
+package com.ispan.CCCMaster.errorcontroller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
