6d68d37da56e37f00e2d96d521b24c3b280b1fe6
FrankHuang <frankcool321@gmail.com>
2023-05-08 11:34:43 +0800

    feat: add BidProductComment model

---
+++ src/main/java/com/ispan/CCCMaster/model/bean/bid/BidProductComment.java
@@ -0,0 +1,86 @@
+package com.ispan.CCCMaster.model.bean.bid;
+
+import com.ispan.CCCMaster.model.bean.customer.Customer;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Entity
+@Table(name = "BidProductComments")
+public class BidProductComment {
+
+    @Id
+    @GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "nvarchar(max)", nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "bidProduct_id", foreignKey = @ForeignKey(name = "fk_bidProductComment_bidProduct"))
+    private BidProduct bidProduct;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", foreignKey = @ForeignKey(name = "fk_bidProductComment_customer"))
+    private Customer customer;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(name = "created_at", columnDefinition = "datetime", nullable = false)
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(name = "updated_at", columnDefinition = "datetime", nullable = false)
+    private Date updatedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public BidProduct getBidProduct() {
+        return bidProduct;
+    }
+
+    public void setBidProduct(BidProduct bidProduct) {
+        this.bidProduct = bidProduct;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
