e05affed5d647d6c640cdd1831ddda9743e1215a
FrankHuang <frankcool321@gmail.com>
2023-05-16 16:22:38 +0800

    refactor: 分開撰寫預設種類與預設拍賣商品

--- src/main/java/com/ispan/CCCMaster/util/GenDefaultData.java
+++ src/main/java/com/ispan/CCCMaster/util/GenDefaultData.java
@@ -4,15 +4,12 @@ import com.github.javafaker.Faker;
 import com.ispan.CCCMaster.model.bean.bid.BidProduct;
 import com.ispan.CCCMaster.model.bean.category.Category;
 import com.ispan.CCCMaster.model.bean.customer.Customer;
-import com.ispan.CCCMaster.model.bean.employee.Employee;
-import com.ispan.CCCMaster.model.bean.employee.Position;
 import com.ispan.CCCMaster.model.dao.BidProductDao;
 import com.ispan.CCCMaster.model.dao.CategoryDao;
 import com.ispan.CCCMaster.model.dao.CustomerDao;
 import com.ispan.CCCMaster.model.dao.EmployeeDao;
 import com.ispan.CCCMaster.model.dao.PositionDao;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -22,7 +19,7 @@ import java.util.List;
 import java.util.Map;
 
 @Component
-public class GenDefaultBidProduct {
+public class GenDefaultData {
 
     private final BidProductDao bidProductDao;
 
@@ -34,11 +31,17 @@ public class GenDefaultBidProduct {
     
     private final EmployeeDao employeeDao;
 
+    private final String[] defaultCategoryNames = new String[]{"手機", "滑鼠", "鍵盤", "電腦", "筆記型電腦"};
+
     private final List<BidProduct> defaultBidProducts = new ArrayList<>();
 
     private final Map<String, Category> categoryMap = new HashMap<>();
 
-    public GenDefaultBidProduct(BidProductDao bidProductDao, CategoryDao categoryDao, CustomerDao customerDao, PositionDao positionDao, EmployeeDao employeeDao) {
+    public GenDefaultData(BidProductDao bidProductDao,
+                          CategoryDao categoryDao,
+                          CustomerDao customerDao,
+                          PositionDao positionDao,
+                          EmployeeDao employeeDao) {
         this.bidProductDao = bidProductDao;
         this.categoryDao = categoryDao;
         this.customerDao = customerDao;
@@ -47,8 +50,46 @@ public class GenDefaultBidProduct {
     }
 
     @PostConstruct
-    public void genDefaultBidProducts() {
+    public void genDefaultDataToDB() {
 
+        // 設定預設種類
+        createCategories();
+
+        // 設定預設拍賣商品
+        createBidProducts();
+        
+        // 設定預設職位資料
+//        Position position1 = new Position();
+//        position1.setPositionId(9999);
+//        position1.setPositionName("Super Manager");
+//        // 設定預設員工資料
+//        Employee employee1 = new Employee();
+//        employee1.setEmployeeName("山西布政司");
+//        employee1.setPosition(position1);
+//        employee1.setPhoneNumber("0999999999");
+//        employee1.setIdNumber("A123456789");
+//        employee1.setPassword("9999");
+//        positionDao.save(position1);
+//        employeeDao.save(employee1);
+    }
+
+    // 預設種類
+    private void createCategories() {
+        long num = categoryDao.count();
+
+        if (num > 0) return;
+
+        // 新增種類
+        for (String name : defaultCategoryNames) {
+            Category category = new Category();
+            category.setName(name);
+            Category newCategory = categoryDao.save(category);
+            categoryMap.put(name, newCategory);
+        }
+    }
+
+    // 預設拍賣商品資料
+    private void createBidProducts() {
         long num = bidProductDao.count();
 
         if (num > 0) return;
@@ -60,11 +101,10 @@ public class GenDefaultBidProduct {
         String computer = "https://images.unsplash.com/photo-1494173853739-c21f58b16055?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80";
         String notebook = "https://images.unsplash.com/photo-1542393545-10f5cde2c810?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80";
         String[] defaultImageLinks = new String[]{cellphone, mouse, keyboard, computer, notebook};
-        String[] defaultCategoryNames = new String[]{"手機", "滑鼠", "鍵盤", "電腦", "筆記型電腦"};
         String[] defaultPrefixes = new String[]{"九成新 ", "全新未拆 ", "限量 ", "贈品轉賣 ", "二手 "};
 
         Faker faker = new Faker();
-        int total = defaultImageLinks.length * 5;
+        int total = defaultImageLinks.length * 10;
 
         // 設定預設 customer
         Customer customer1 = new Customer();
@@ -82,7 +122,7 @@ public class GenDefaultBidProduct {
 
         for (int i = 0; i < total; i += 1) {
             BidProduct bidProduct = new BidProduct();
-            int index = i / 5;
+            int index = i / 11; // 使 index 界於 0 - 4
             int random = (int) Math.floor(Math.random() * defaultPrefixes.length);
             String randomPrefix = defaultPrefixes[random];
             bidProduct.setName(randomPrefix + defaultCategoryNames[index]);
@@ -92,7 +132,7 @@ public class GenDefaultBidProduct {
             bidProduct.setDescription(faker.lorem().paragraph());
             bidProduct.setImage(defaultImageLinks[index]);
             if (i % 5 == 0) {
-                bidProduct.setCustomer(customer1);
+                bidProduct.setCustomer(customer1); // 給 customer 1
             } else {
                 bidProduct.setCustomer(customer2);
             }
@@ -100,20 +140,6 @@ public class GenDefaultBidProduct {
         }
 
         bidProductDao.saveAll(defaultBidProducts);
-        
-        // 設定預設職位資料
-        Position position1 = new Position();
-        position1.setPositionId(9999);
-        position1.setPositionName("Super Manager");
-        // 設定預設員工資料
-        Employee employee1 = new Employee();
-        employee1.setEmployeeName("山西布政司");        
-        employee1.setPosition(position1);
-        employee1.setPhoneNumber("0999999999");
-        employee1.setIdNumber("A123456789");        
-        employee1.setPassword("9999");
-        positionDao.save(position1);
-        employeeDao.save(employee1);
     }
 
     private Category getOrCreateCategory(String categoryName) {
